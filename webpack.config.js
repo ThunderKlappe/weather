9b9db43718c18506072d3a0eaf160b20ebd73de0
ThunderKlappe <timschley@gmail.com>
2022-03-02 15:02:44 -0800

    Set up website

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-undef */
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: {
+        index: "./src/index.js",
+    },
+    devtool: "inline-source-map",
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Basic Sign-up Form",
+            template: "src/index.html",
+        }),
+    ],
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [["@babel/preset-env", { targets: "defaults" }]],
+                        plugins: ["@babel/plugin-proposal-class-properties"],
+                    },
+                },
+            },
+        ],
+    },
+};
